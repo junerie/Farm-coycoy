ab63634a4c14221f2f9ae948066d95dd3b0c3ccf
junerie <yjunerie@gmail.com>
2023-10-06 21:40:04 +0800

    folder img add sample json

--- script.js
+++ script.js
@@ -34,3 +34,27 @@ $(document).ready(function(){
         zoom: 8
       });
     }
+
+    // product chart 
+    const imgs=document.querySelectorAll('.img a');
+let imgId=1;
+
+const imgDiv=document.querySelectorAll('.img');
+
+imgs.forEach((img)=>{
+  img.addEventListener('click',(e)=>{
+    e.preventDefault();
+    imgId=img.dataset.id;
+
+    imgDiv.forEach((img)=>{
+      img.classList.remove('active');
+    });
+
+    img.parentElement.classList.add('active');
+
+    moveImage();
+  });
+});
+
+
+
